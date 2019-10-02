57dfe6a08641261907d162f4634a7894cf02e33a
Alan Wolfe <alan.wolfe@gmail.com>
2019-10-01 21:36:39 -0700

    floats, srgb, histogram, reshaped triangle noise

--- main.cpp
+++ main.cpp
@@ -13,8 +13,6 @@
 typedef uint8_t uint8;
 typedef uint32_t uint32;
 
-
-
 #define DETERMINISTIC() true  // if true, will use the seed below for everything, else will randomly generate a seed.
 #define DETERMINISTIC_SEED() unsigned(783104853), unsigned(4213684301), unsigned(3526061164), unsigned(614346169), unsigned(478811579), unsigned(2044310268), unsigned(3671768129), unsigned(206439072)
 
@@ -44,43 +42,110 @@ T Clamp(T value, T min, T max)
     else
         return value;
 }
-uint8 GradientValueU8(size_t pixelX, size_t pixelY)
+
+template <typename T>
+T sign(T value)
 {
-    float value = float(pixelX) / float(c_gradientWidth - 1);
-    return (uint8)Clamp(value * 255.0f + 0.5f, 0.0f, 255.0f);
+    if (value < T(0))
+        return T(-1);
+    else if (value == T(0))
+        return T(0);
+    else
+        return T(1);
+}
+
+// sRGB routines from https://www.nayuki.io/res/srgb-transform-library/srgb-transform.c
+float srgb_to_linear_float(float x) {
+    if (x <= 0.0f)
+        return 0.0f;
+    else if (x >= 1.0f)
+        return 1.0f;
+    else if (x < 0.04045f)
+        return x / 12.92f;
+    else
+        return powf((x + 0.055f) / 1.055f, 2.4f);
+}
+
+// sRGB routines from https://www.nayuki.io/res/srgb-transform-library/srgb-transform.c
+float linear_to_srgb_float(float x) {
+    if (x <= 0.0f)
+        return 0.0f;
+    else if (x >= 1.0f)
+        return 1.0f;
+    else if (x < 0.0031308f)
+        return x * 12.92f;
+    else
+        return powf(x, 1.0f / 2.4f) * 1.055f - 0.055f;
 }
 
 // Assumes src and dest have the same width
-void AppendImageVertical(std::vector<uint8>& dest, const std::vector<uint8>& src)
+void AppendImageVertical(std::vector<float>& dest, const std::vector<float>& src)
 {
     size_t destSize = dest.size();
     size_t srcSize = src.size();
     dest.resize(destSize + srcSize);
-    memcpy(&dest[destSize], src.data(), srcSize);
+    memcpy(&dest[destSize], src.data(), srcSize * sizeof(src[0]));
+}
+
+void SaveImage(const char* fileName, const std::vector<float>& image, size_t width, size_t height)
+{
+    std::vector<uint8> outImage(image.size());
+    for (size_t index = 0; index < image.size(); ++index)
+    {
+        float sRGB = linear_to_srgb_float(image[index]);
+        outImage[index] = uint8(Clamp(sRGB * 255.0f + 0.5f, 0.0f, 255.0f));
+    }
+
+    stbi_write_png(fileName, int(width), int(height), 1, outImage.data(), 0);
 }
 
 template <typename LAMBDA>
-void DoTest(const char* fileName, const std::vector<uint8>& gradient, const LAMBDA& lambda)
+void DoTest(const char* fileName, const std::vector<float>& gradient, const LAMBDA& lambda)
 {
-    std::vector<uint8> gradientDithered(c_gradientWidth*c_gradientHeight);
-    std::vector<uint8> gradientError(c_gradientWidth*c_gradientHeight);
+    std::vector<float> gradientDithered(c_gradientWidth*c_gradientHeight);
+    std::vector<float> gradientError(c_gradientWidth*c_gradientHeight);
+    std::vector<float> gradientHistogram(c_gradientWidth*c_gradientHeight);
+    std::vector<uint32> histogram(c_gradientWidth, 0);
+
     for (size_t iy = 0; iy < c_gradientHeight; ++iy)
     {
         for (size_t ix = 0; ix < c_gradientWidth; ++ix)
         {
-            uint8 randValue = lambda(ix, iy);
-            gradientDithered[iy*c_gradientWidth + ix] = (gradient[iy*c_gradientWidth + ix] > randValue) ? 255 : 0;
-            if (gradient[iy*c_gradientWidth + ix] >= randValue)
-                gradientError[iy*c_gradientWidth + ix] = gradient[iy*c_gradientWidth + ix] - randValue;
-            else
-                gradientError[iy*c_gradientWidth + ix] = randValue - gradient[iy*c_gradientWidth + ix];
+            // get random value
+            float randValue = lambda(ix, iy);
+
+            // dither
+            gradientDithered[iy*c_gradientWidth + ix] = (gradient[iy*c_gradientWidth + ix] > randValue) ? 1.0f : 0.0f;
+
+            // error image
+            gradientError[iy*c_gradientWidth + ix] = std::abs(gradient[iy*c_gradientWidth + ix] - randValue);
+
+            // histogram
+            size_t histogramBucket = Clamp<size_t>(size_t(randValue * (c_gradientWidth - 1) + 0.5f), 0, c_gradientWidth - 1);
+            histogram[histogramBucket]++;
         }
     }
 
-    std::vector<uint8> outImage = gradient;
+    // make histogram image
+    uint32 maxHistogramValue = histogram[0];
+    for (uint32 histogramValue : histogram)
+        maxHistogramValue = std::max(maxHistogramValue, histogramValue);
+
+    for (size_t iy = 0; iy < c_gradientHeight; ++iy)
+    {
+        for (size_t ix = 0; ix < c_gradientWidth; ++ix)
+        {
+            float normalizedHistogramValue = 1.0f - float(histogram[ix]) / float(maxHistogramValue);
+            size_t height = Clamp<size_t>(size_t(normalizedHistogramValue * (c_gradientHeight - 1) + 0.5), 0, c_gradientHeight-1); // TODO: pad from top?
+            gradientHistogram[iy*c_gradientWidth + ix] = (iy < height) ? 1.0f : 0.0f;
+        }
+    }
+
+    std::vector<float> outImage = gradient;
     AppendImageVertical(outImage, gradientDithered);
     AppendImageVertical(outImage, gradientError);
-    stbi_write_png(fileName, c_gradientWidth, c_gradientHeight * 3, 1, outImage.data(), 0);
+    AppendImageVertical(outImage, gradientHistogram);
+    SaveImage(fileName, outImage, c_gradientWidth, c_gradientHeight * 4);
 }
 
 int main(int argc, char** argv)
@@ -88,32 +153,56 @@ int main(int argc, char** argv)
     // init random number generator
     std::mt19937 rng(GetRNGSeed());
 
-    // make the gradient image
-    std::vector<uint8> gradient(c_gradientWidth*c_gradientHeight);
+    // make the (linear) gradient image
+    std::vector<float> gradient(c_gradientWidth*c_gradientHeight);
     {
         for (size_t iy = 0; iy < c_gradientHeight; ++iy)
             for (size_t ix = 0; ix < c_gradientWidth; ++ix)
-                gradient[iy*c_gradientWidth + ix] = GradientValueU8(ix, iy);
+                gradient[iy*c_gradientWidth + ix] = float(ix) / float(c_gradientWidth - 1);
     }
 
     // uniform white noise test
     DoTest("out_gradient_white_uniform.png", gradient,
         [&rng] (size_t ix, size_t iy)
         {
-            static std::uniform_int_distribution<uint32> dist(0, 255);
-            return uint8(dist(rng));
+            static std::uniform_real_distribution<float> dist;
+            return dist(rng);
         }
     );
 
-    // triangular white noise test
+    // triangular white noise test, made by averaging two white noise values
     DoTest("out_gradient_white_triangle_avg.png", gradient,
         [&rng] (size_t ix, size_t iy)
         {
-            static std::uniform_int_distribution<uint32> dist(0, 255);
-            return uint8((dist(rng) + dist(rng))/2);
+            static std::uniform_real_distribution<float> dist;
+            return (dist(rng) + dist(rng)) / 2;
         }
     );
 
+    // triangular white noise test, made by reshaping a single white noise value
+    // https://www.shadertoy.com/view/4t2SDh
+    DoTest("out_gradient_white_triangle_reshape.png", gradient,
+        [&rng] (size_t ix, size_t iy)
+        {
+            static std::uniform_real_distribution<float> dist;
+            float nrnd0 = dist(rng);
+
+            // Convert uniform distribution into triangle-shaped distribution.
+            float orig = nrnd0 * 2.0f - 1.0f;
+            nrnd0 = orig * 1.0f / sqrt(abs(orig));
+            nrnd0 = std::max(-1.0f, nrnd0); // Nerf the NaN generated by 0*rsqrt(0). Thanks @FioraAeterna!
+            nrnd0 = nrnd0 - sign(orig) + 0.5f;
+
+            // Result is range [-0.5,1.5] which is
+            // useful for actual dithering.
+            // convert to [0,1] for histogram.
+            return (nrnd0 + 0.5f) * 0.5f;
+        }
+    );
+    // TODO: try working through inverting the CDF for triangle distribution
+
+    // TODO: apparently the -0.5 to +1.5 is useful for dithering?? understand that!
+    // TODO: need to dither in linear space, so need to work in floats!
     // TODO: show an image for error, and another for histogram.
     // TODO: test multiple quantizations? not just 1 bit? i think thats where the triangular noise helps
     // TODO: inside used triangle noise that went outside 0 to 1. is that important?
@@ -147,7 +236,11 @@ TODO:
 
 
 
+Notes:
+* working in float because we need to dither in linear color space.
 
+
+Future:
 ! DFT circle by packing hexagons?
 
 */
